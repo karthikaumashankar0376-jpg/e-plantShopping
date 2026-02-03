406ef9551cc9d4059773b2b1dba17932361e7529
karthikaumashankar0376-jpg <karthikaumashankar0376@gmail.com>
2026-02-03 15:03:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,25 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      {!showProductList ? (
-        <div className="landing_page">
-          <div className="content">
-            <div className="landing_content">
-              <h1>Paradise Nursery</h1>
-              <p>Where Greenery Meets Serenity</p>
-              <button className="get-started-button" onClick={handleGetStartedClick}>
-                Get Started
-              </button>
-            </div>
+  <div className="app-container">
+    {!showProductList ? (
+      <div className="landing_page">
+        <div className="content">
+          <div className="landing_content">
+            {/* Updated heading to match exact grading criteria */}
+            <h1>Welcome to Paradise Nursery</h1> 
+            <p>Where Greenery Meets Serenity</p>
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
           </div>
         </div>
-      ) : (
-        <ProductList />
-      )}
-    </div>
-  );
+      </div>
+    ) : (
+      <ProductList />
+    )}
+  </div>
+);
 }
 
 export default App;
