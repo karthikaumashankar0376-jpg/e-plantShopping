521af9448b46c4eaf51b1521e7ed4dfc8b3944e7
karthikaumashankar0376-jpg <karthikaumashankar0376@gmail.com>
2026-02-03 14:51:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,33 @@
-
 import React, { useState } from 'react';
-import ProductList from './ProductList';
 import './App.css';
-import AboutUs from './AboutUs';
+import ProductList from './ProductList';
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
     setShowProductList(true);
   };
 
-  const handleHomeClick = () => {
-    setShowProductList(false);
-  };
-
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-        <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
+      {!showProductList ? (
+        <div className="landing_page">
+          <div className="content">
+            <div className="landing_content">
+              <h1>Paradise Nursery</h1>
+              <p>Where Greenery Meets Serenity</p>
+              <button className="get-started-button" onClick={handleGetStartedClick}>
+                Get Started
+              </button>
+            </div>
           </div>
-          </div>
-
-      </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList onHomeClick={handleHomeClick}/>
-      </div>
+        </div>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 }
 
 export default App;
-
-
-
